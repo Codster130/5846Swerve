d0790e98607521c3d2113f09e7abac9913193f2f
JackKertscher <jackkertscher@gmail.com>
2022-02-25 12:28:15 -0500

    Turret PID

--- Better Swerve Code/src/main/java/frc/robot/subsystems/Turret.java
+++ Better Swerve Code/src/main/java/frc/robot/subsystems/Turret.java
@@ -4,25 +4,35 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import frc.robot.Constants;
 
 public class Turret extends PIDSubsystem {
+  CANSparkMax turretMotor = new CANSparkMax(Constants.turretMotorID, MotorType.kBrushless);
+  RelativeEncoder turretEncoder = turretMotor.getEncoder();
   /** Creates a new Turret. */
   public Turret() {
     super(
         // The PIDController used by the subsystem
         new PIDController(0, 0, 0));
+    getController().setTolerance(1, 1);
+    setSetpoint(0);
   }
 
   @Override
   public void useOutput(double output, double setpoint) {
     // Use the output here
+    turretMotor.set(output);
   }
 
   @Override
   public double getMeasurement() {
     // Return the process variable measurement here
-    return 0;
+    return turretEncoder.getPosition();
   }
 }
